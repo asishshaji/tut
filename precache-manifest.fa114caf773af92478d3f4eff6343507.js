abe3bde60834d08139fecc8eabf51a6839c2cd4b
mrkai1253 <asishshajithomas@gmail.com>
2020-05-17 02:29:29 +0530

    Updates

---
+++ precache-manifest.fa114caf773af92478d3f4eff6343507.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eaad29e71eaa998efb1",
+    "url": "/tut/static/js/app.d40973d2.chunk.js"
+  },
+  {
+    "revision": "f1fc1e91602f1ca832ab",
+    "url": "/tut/static/js/runtime~app.53f897d2.js"
+  },
+  {
+    "revision": "523e5aefc58e7e9f4096",
+    "url": "/tut/static/js/2.bea67fa7.chunk.js"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/tut/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "a19a7b108b2e3961fc855c6ea5a6546f",
+    "url": "/tut/./fonts/ProductSans-Bold.ttf"
+  },
+  {
+    "revision": "9c51beb79b8ab173abd924ce39178f0b",
+    "url": "/tut/./fonts/ProductSans-Medium.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/tut/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/tut/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/tut/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/tut/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/tut/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/tut/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/tut/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/tut/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/tut/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/tut/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/tut/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/tut/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/tut/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/tut/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/tut/favicon.ico"
+  },
+  {
+    "revision": "e007c883dd244b3062f8ef844c7b514c",
+    "url": "/tut/index.html"
+  },
+  {
+    "revision": "2ffc642e2ac07065658bb1eb69bf9afb",
+    "url": "/tut/manifest.json"
+  },
+  {
+    "revision": "847ed62c9f5e22fee1e9c6b6469736a6",
+    "url": "/tut/register-service-worker.js"
+  }
+];
